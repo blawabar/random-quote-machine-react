a71aab29d3b687511dcc7c2627a7af6b0a571847
blawabar <fake.torrent@wp.pl>
2020-01-27 21:12:45 +0100

    add mechanism for dynamically change view content with use of matchMedia and mediaQueryList features

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,23 +1,57 @@
 import React from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 
 import "./QuoteCard.scss";
 
 const QuoteCard = ({ content, author }) => {
-  const computeFontSizeStyle = () => {
-    const mediaQuerList = matchMedia("screen and (max-width: 640px");
-    let style = null;
+  const mediaQueryList = useRef(
+    matchMedia("screen and (max-width: 667px) and (max-height: 568px)")
+  );
 
-    if (content.length >= 225 && mediaQuerList.matches) {
-      style = { fontSize: "16px" };
-    }
+  const getView = useCallback(
+    shouldBeShrinked => {
+      let view = "normal";
 
-    return style;
-  };
+      if (content.length >= 225 && shouldBeShrinked) {
+        view = "shrinked";
+      }
+
+      return view;
+    },
+    [content]
+  );
+
+  const [view, setView] = useState(() =>
+    getView(mediaQueryList.current.matches)
+  );
+
+  const getSentenceStyle = useCallback(
+    () =>
+      view === "shrinked" ? { fontSize: "16px", marginBottom: "10px" } : null,
+    [view]
+  );
+
+  const handleChangeView = useCallback(mqle => setView(getView(mqle.matches)), [
+    getView
+  ]);
+
+  useEffect(() => {
+    const mql = mediaQueryList.current;
+
+    mql.addEventListener("change", handleChangeView);
+
+    return () => mql.removeEventListener("change", handleChangeView);
+  }, [handleChangeView]);
 
   return (
     <div className="quote-card">
+      <img
+        className="quote-card__quote-sign"
+        alt="quote sign"
+        src="quote.svg"
+      />
       <section className="quote-card__body">
-        <p className="quote-card__sentence" style={computeFontSizeStyle()}>
+        <p className="quote-card__sentence" style={getSentenceStyle()}>
           {content}
         </p>
         <p className="quote-card__author-name">{author}</p>
