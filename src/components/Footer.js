85f716a22cc7a89e5fa5d267e707f6fd783077f6
blawabar <fake.torrent@wp.pl>
2020-01-27 22:51:38 +0100

    add info and styling for the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,32 @@
 import React from "react";
 import "./Footer.scss";
 
+const quotableLink = "https://github.com/lukePeavey/quotable";
+const apiName = "Random Quotes API";
+
+const lukePeaveyLink = "https://github.com/lukePeavey";
+const apiAuthorName = "Luke Peavey";
+
 const Footer = () => {
+  const generateLink = (addr, text) => (
+    <a
+      className="footer__external-link"
+      href={addr}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {text}
+    </a>
+  );
+
   return (
     <div className="footer">
-      <h1>Footer</h1>
+      <p className="footer__app-name">Random Quote Machine by Bart Krolak</p>
+      <p className="footer__api-info">
+        {generateLink(quotableLink, apiName)}
+        {" by "}
+        {generateLink(lukePeaveyLink, apiAuthorName)}
+      </p>
     </div>
   );
 };
